34c92bf3f26a2b9a72c54e20883057135c5cb28b
Utah Kate Newman <utah.kate.newman@gmail.com>
2015-05-11 15:11:40 -0700

    generate selected class in navbar

--- main.rb
+++ main.rb
@@ -1,13 +1,27 @@
 require 'sinatra'
 
 get '/' do
+  @index = "selected"
+  @resume = ""
+  @projects = ""
   erb :index
 end
 
 get '/resume' do
+  @index = ""
+  @resume = "selected"
+  @projects = ""
   erb :resume
 end
 
 get '/projects' do
+  @index = ""
+  @resume = ""
+  @projects = "selected"
   erb :projects 
+end
+
+# this renders header and general page formatting, as well as the text 'operationstormthecastle.com'
+get '/blog' do
+  erb "operationstormthecastle.blogspot.com", :blog => :post
 end
