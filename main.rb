64c53a5c69692371f0c502a8eb97bd44095f0bb4
Utah Kate Newman <utah.kate.newman@gmail.com>
2017-04-28 23:00:27 -0700

    position animation lines

--- main.rb
+++ main.rb
@@ -1,24 +1,6 @@
 require 'sinatra'
 require 'sass'
 
-# class MyApp < Sinatra
-#   #initialize new sprockets environment
-#   set :environment, Sprockets::Environment.new
-
-#   # append assets paths
-#   environment.append_path "assets/styles"
-#   environment.append_path "assets/js"
-
-#   # compress assets
-#   environment.js_compressor  = :uglify
-#   environment.css_compressor = :scss
-
-#   # get assets
-#   get "/assets/*" do
-#     env["PATH_INFO"].sub!("/assets", "")
-#     settings.environment.call(env)
-#   end
-
 get '/' do
   @index = "selected"
   @resume = ""
@@ -39,9 +21,3 @@ get '/projects' do
   @projects = "selected"
   erb :projects
 end
-
-# end
-
-
-
-
